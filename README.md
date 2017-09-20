f174eff555524a8a1c0a24de0e359acc82e52f5d
ars <ars@git.com>
2017-09-20 16:24:58 +0530

    Added the required files.

---
+++ README.md
@@ -0,0 +1,43 @@
+# ARS Birthday Cal App
+
+This is a simple app which reads the json data from the textarea and the year entered by the user and accordingly plots the birthday in the proper day card with the initials of the name and the count of the birthdays for the particular day.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See how to run section for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+You need to have a web browser installed on your machine to view the site.
+
+### Installing
+
+In order to install chrome you can refer the following site and follow the installation process.
+[Google Support](https://support.google.com/chrome/answer/95346?co=GENIE.Platform%3DDesktop&hl=en-GB)
+
+## How to Run
+
+* In order to see the website you just need to open the `index.html` file in your browser.
+* If you dont have the json data you can use the sample data in `birthdays.json`.
+Sample json data format
+[
+    {
+      "name": "Tyrion Lannister",
+      "birthday": "12/02/1978"
+    }, {
+      "name": "Cersei Lannister",
+      "birthday": "11/30/1975"
+    }
+]    
+
+* The birthday attribute in the above data is in the format of mm/dd/YYYY.
+
+
+## Authors
+
+* **Ankush Sankhe** - *HTML, CSS and JavaScript scripting* 
+
+## Acknowledgments
+
+* [favicon image](https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png)
+* Hat tip to anyone who's code was used.
---
+++ js/main.js
@@ -0,0 +1,167 @@
+var action = 1;
+var myLink = document.getElementById('myLink');
+
+myLink.onclick = function magicFlow() {
+
+  var validInputs = yearRequired() && jsonData() &&
+    jsonDataValidator();
+
+  if (validInputs) {
+    if (action == 1) {
+
+      var jsonObj1 = document.getElementById("myTextarea").value;
+      var jsonObj = JSON.parse(jsonObj1);
+
+      var yearText = document.getElementById("yearVal").value;
+      setDayName(yearText, ...jsonObj);
+      getCount();
+
+      function getCount() {
+        var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+
+        for (var i = 0; i < weekday.length; i++) {
+          var container_div = document.getElementById(weekday[i]);
+          var count = container_div.getElementsByTagName('div').length;
+          if (count == 1) {
+            var fragment = create(count + " birthday");
+          }
+          else if (count == 0){
+            var fragment = create("No birthdays");
+          } else {
+            var fragment = create(count + " birthdays");
+          }
+          document.getElementById(weekday[i] + "Count").appendChild(
+            fragment);
+        }
+      }
+      action = 2;
+
+    } else {
+      window.location.reload();
+      action = 1;
+    }
+  } else {
+    console.log("Please Enter the required data.");
+  }
+
+  function yearRequired() {
+    if (document.getElementById('yearVal').value == "") {
+      if (confirm("Press enter a year.") == true) {
+        document.getElementById('yearVal').style.border =
+          "thin solid red"
+      } else {
+        console.log("You pressed Cancel");
+      }
+      return false;
+    } else {
+      document.getElementById('yearVal').style.border =
+        "thin solid #3d3d3d"
+      return true;
+    }
+  }
+
+  function jsonData() {
+    var jsonObj1 = document.getElementById("myTextarea").value;
+    if (jsonObj1 == "") {
+      if (confirm("Please Enter json data") == true) {
+        document.getElementById('myTextarea').style.border =
+          "thin solid red";
+      } else {
+        console.log("You pressed Cancel!");
+      }
+      return false;
+    } else {
+      document.getElementById('myTextarea').style.border =
+        "thin solid #3d3d3d";
+      return true;
+    }
+  }
+
+  function setUsername(id, newUsername) {
+    for (var i = 0; i < jsonObj.length; i++) {
+      if (jsonObj[i].Id === id) {
+        jsonObj[i].Username = newUsername;
+        return;
+      }
+    }
+  }
+
+  function getDay(ds) {
+    var d = new Date(ds);
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var n = weekday[d.getDay()];
+    return n;
+  }
+
+  function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+      temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+      frag.appendChild(temp.firstChild);
+    }
+    return frag;
+  }
+
+  function setDayName(yearNumber, ...jsonObj) {
+    for (var i = 0; i < jsonObj.length; i++) {
+      var pos = jsonObj[i].birthday.indexOf("/");
+      var monthNumber = jsonObj[i].birthday.substr(0, pos);
+      var yearNum = jsonObj[i].birthday.lastIndexOf("/");
+      var dnum = jsonObj[i].birthday.substr(pos + 1, 2);
+      var ds = yearNumber + "-" + monthNumber + "-" + dnum;
+      var dayName = getDay(ds);
+      jsonObj[i].Day = dayName;
+      var spos = jsonObj[i].name.indexOf(" ");
+      var fI = jsonObj[i].name;
+      fI = fI.charAt(0).toUpperCase();
+      var lI = jsonObj[i].name.substr(spos + 1, 2);
+      lI = lI.charAt(0).toUpperCase();
+      var initials = fI + lI;
+      var dId = 'posts' + initials;
+      var fragment = create('<div id="' + dId +
+        '" class="posts">' + initials + '</div>');
+      document.getElementById(dayName).appendChild(fragment);
+      ran_col(dId);
+    }
+  }
+
+  function ran_col(IdTar) { //function name
+    var color = '#'; // hexadecimal starting symbol
+    var letters = ['464D56', '70B456', '9656B4', 'B4567A',
+      'F90F47', '849E0D', 'F9440F', '0FF2F9', '0D0D9E',
+      '022B2E', '9510A2', '693803', '585857'
+    ]; //Set your colors here
+    color += letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById(IdTar).style.backgroundColor =
+      color;
+  }
+
+  function jsonDataValidator() {
+    var js = document.getElementById("myTextarea").value;
+    try {
+      JSON.parse(js);
+    } catch (e) {
+      console.log(e)
+      alert("Invalid JSON\nError = " + e);
+      return false;
+    }
+    return true;
+  }
+
+}
